a36bcfc8c6a64c68942b27b2bf94b0cd1919ee79
OmSuryawanshi9011 <131766249+OmSuryawanshi9011@users.noreply.github.com>
2025-01-27 14:05:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+function getNumbers() {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve([1, 2, 3, 4, 5, 6]);
+		}, 3000)
+	});
+}
+getNumbers().then(numbers => {
+	return new Promise((resolve, reject) => {
+		const evenNumbers = numbers.filter(number => number % 2 === 0);
+		resolve(evenNumbers);
+	});
+}).then(evenNumbers => {
+	return new Promise((resolve, reject) => {
+		const doubledNumbers = evenNumbers.map(number => number * 2);
+		resolve(doubledNumbers);
+	});
+}).then(doubledNumbers => {
+	const outputElement = document.getElementById('output');
+	outputElement.textContent = doubledNumbers.join(', ');
+});
